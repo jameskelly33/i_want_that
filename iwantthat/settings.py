ecb1325f408c35468bf106c7442be2e843992f99
James Kelly <jameskelly33@gmail.com>
2023-11-26 07:50:58 +0000

    Update requirements

--- iwantthat/settings.py
+++ iwantthat/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -29,9 +29,9 @@ load_dotenv()
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False').lower() == "true"
 
-ALLOWED_HOSTS = ['192.168.0.210', '127.0.0.1' ]
+ALLOWED_HOSTS = os.environ.get("ALLOWED HOSTS").split(" ")
 
 
 # Application definition
@@ -93,8 +93,8 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-
+database_url = os.environ.get("DATABASE_URL")
+DATABASES['default'] = dj_database_url.parse(database_url)
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
