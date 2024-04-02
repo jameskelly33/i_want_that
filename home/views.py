c1a62a0bb282d395a8e31ce32c59a27e019ffca4
James Kelly <james@Jamess-MacBook-Pro.local>
2024-04-02 22:26:52 +0100

    test email

--- home/views.py
+++ home/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
-
+from django.core.mail import send_mail
 # Create your views here.
 def home(request):
+    
     return render(request,'home/index.html')
+
+
+
+
+send_mail(
+    "Subject here",
+    "Here is the message.",
+    "jameskelly33@gmail.com",
+    ["jameskelly33@gmail.com"],
+    fail_silently=False,
+)
