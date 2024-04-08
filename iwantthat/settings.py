051189c758456ebdce6803d2e013a68acbda2b15
James Kelly <james@Jamess-MacBook-Pro.local>
2024-04-08 21:45:37 +0100

    add email

--- iwantthat/settings.py
+++ iwantthat/settings.py
@@ -152,4 +152,10 @@ CRISPY_TEMPLATE_PACK='bootstrap5'
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'jameskelly33@gmail.com'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 
